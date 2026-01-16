50fc0a9290c2b37c24e26cd3b58a78a027c83c2e
ReemAlshehri04 <re2malshehri@gmail.com>
2026-01-17 02:41:09 +0300

    done

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Print numbers from 1 to 100 with FizzBuzz rules."""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
+
+# Call the function
+if __name__ == "__main__":
+    fizzbuzz()
