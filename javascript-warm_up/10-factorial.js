5fe576501bed17529ada45ea859e76314d49d1a2
ReemAlshehri04 <re2malshehri@gmail.com>
2026-03-11 08:26:34 +0000

    done

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function factorial(n) 
-{
-  if (isNaN(n) || n <= 1) 
- {
+function factorial (n) {
+  if (isNaN(n) || n === 0 || n === 1) {
     return 1;
- }
-return n * factorial(n - 1);
- }
-const num = parseInt(process.argv[2]);
-console.log(factorial(num));
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const n = parseInt(process.argv[2]);
+console.log(factorial(n));
